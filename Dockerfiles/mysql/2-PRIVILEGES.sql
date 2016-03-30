8d56d70b888b2278a4f540a58149738bcaa313f4
Paul Girault-Matz <pgmatz@excilys.com>
2016-03-30 18:28:00 +0200

    modif sql privileges

--- Dockerfiles/mysql/2-PRIVILEGES.sql
+++ Dockerfiles/mysql/2-PRIVILEGES.sql
@@ -1,9 +1,9 @@
 #-----------------------------------
 #USER RIGHTS MANAGEMENT
 #-----------------------------------
-CREATE USER 'admincdb'@'172.25.0.%' IDENTIFIED BY 'qwerty1234';
+CREATE USER 'admincdb'@'172.18.0.%' IDENTIFIED BY 'qwerty1234';
 CREATE USER 'admincdb'@'localhost' IDENTIFIED BY 'qwerty1234';
 
-GRANT ALL PRIVILEGES ON `computer-database-db`.* TO 'admincdb'@'172.25.0.%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `computer-database-db`.* TO 'admincdb'@'172.18.0.%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON `computer-database-db`.* TO 'admincdb'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
